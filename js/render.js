7c04f13c658209f81f587cd36781bd58c4090a17
OwaisPatel07 <owaispatel07@gmail.com>
2026-04-01 16:03:00 +0530

    initial commit

---
+++ js/render.js
@@ -0,0 +1,89 @@
+// Header Hamburger menu Open/Close Functionality Start 
+
+var hamburgerBtn = document.querySelector('.hamburger-button');
+var closeBtn = document.querySelector('.close-btn');
+var backDrop = document.querySelector('.backDrop');
+
+hamburgerBtn.addEventListener('click', function() {
+    backDrop.classList.add('pop-up-show');
+    document.body.classList.add('scroll-y');
+});
+
+closeBtn.addEventListener('click', function() {
+    backDrop.classList.remove('pop-up-show');
+    document.body.classList.remove('scroll-y');
+});
+
+// Header Hamburger menu Open/Close Functionality End 
+
+// Banner Slick Slider Initialization Start
+
+$(document).ready(function(){
+    $('.banner-wrapper').slick({
+        dots: true,
+        arrows: true,
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        pauseOnHover: true,
+        cssEase: 'cubic-bezier(0.645, 0.045, 0.355, 1)'
+    });
+});
+
+// Banner Slick Slider Initialization End
+
+// Product Tabs Functionality Start
+var productTabs = document.querySelectorAll('.productTabs');
+
+productTabs.forEach(function(tab) {
+    tab.addEventListener('click', function() {
+        productTabs.forEach(function(tab) {
+            tab.classList.remove('active');
+        });
+        this.classList.add('active');
+    });
+});
+
+
+// Product Tabs Functionality End
+
+// Product Content Slick Slider Initialization Start
+
+// Kitchen Content Slick Slider
+$(document).ready(function(){
+    $('.kitchen-content-slick').slick({
+        dots: true,
+        arrows: true,
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        pauseOnHover: true,
+        fade: true,
+        cssEase: 'linear'
+    });
+});
+
+// Wardrobe Content Slick Slider
+$(document).ready(function(){
+    $('.wardrobe-content-slick').slick({
+        dots: true,
+        arrows: true,
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        pauseOnHover: true,
+        fade: true,
+        cssEase: 'linear'
+    });
+});
+
+// Product Content Slick Slider Initialization End
